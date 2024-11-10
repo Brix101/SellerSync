3024c31ca8cde42e08b9a2c11f560c35f32db9bb
Brixter Porras <brixterporras@gmail.com>
2024-11-10 10:48:10 +0800

    Initial Commit

---
+++ src/main/java/com/brix/Seller_Sync/SellerSyncApplication.java
@@ -0,0 +1,13 @@
+package com.brix.Seller_Sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerSyncApplication.class, args);
+	}
+
+}
