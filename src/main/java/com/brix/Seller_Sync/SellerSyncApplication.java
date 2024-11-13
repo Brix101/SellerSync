11b245559c4f634815f280d64785bd080ab34c10
Brixter Porras <brixterporras@gmail.com>
2024-11-13 11:38:41 +0800

    feat: added initial product shedule task cron

--- src/main/java/com/brix/Seller_Sync/SellerSyncApplication.java
+++ src/main/java/com/brix/Seller_Sync/SellerSyncApplication.java
@@ -2,8 +2,10 @@ package com.brix.Seller_Sync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SellerSyncApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/brix/Seller_Sync/product/ProductScheduledTasks.java
@@ -0,0 +1,24 @@
+
+package com.brix.Seller_Sync.product;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.java.Log;
+
+@Component
+@Log
+public class ProductScheduledTasks {
+
+    @Scheduled(cron = "0 0 0 * * ?") // This cron expression means every day at midnight
+    public void performProductCron() {
+        // Your product cron task logic here
+        log.info("Product cron task executed");
+    }
+
+    @Scheduled(cron = "*/30 * * * * ?") // This cron expression means every 30 seconds
+    public void performTask() {
+        // Your task logic here
+        log.info("Product cron task executed every 30 seconds");
+    }
+}
