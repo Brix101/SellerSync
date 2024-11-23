4267d0abcb45c80fbfb4d5689c98872bf39d3ea9
Brixter Porras <brixterporras@gmail.com>
2024-11-23 17:43:27 +0800

    feat: added global report que

---
+++ src/main/java/com/brix/Seller_Sync/reportqueue/ReportQueueService.java
@@ -0,0 +1,19 @@
+package com.brix.Seller_Sync.reportqueue;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.brix.Seller_Sync.amzn.payload.CreateReportSpecification.ReportType;
+
+public interface ReportQueueService {
+
+    void enqueueReport(ReportQueue reportQueueObj, String value);
+
+    void dequeueReport(String key);
+
+    ConcurrentHashMap<String, String> getQueuedReports(ReportType reportType);
+
+    Boolean isReportInQueue(ReportQueue reportQueueObj);
+
+    String getClientIdFromKey(String key);
+
+}
